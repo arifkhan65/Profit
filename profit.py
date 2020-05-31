320d4c80ad5ba5667e8446f5afe6cf7c50b0eb7b
Arif <arifkhan65@gmail.com>
2020-05-31 15:30:43 +0200

    Initial commit

---
+++ profit.py
@@ -0,0 +1,8 @@
+
+buying=float(input('enter buying price: '))
+selling=float(input('enter selling price: '))
+result=selling-buying
+if result >=00.0:
+    print('profit is',result)
+else:
+    print('loss is:', result)
